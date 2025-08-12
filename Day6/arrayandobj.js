664b13b50a560365d20e8b0a17c0cade6bdf3138
GOPINAATH15 <gopinaath515@gmail.com>
2025-08-12 13:17:31 +0530

    array and obj

---
+++ Day6/arrayandobj.js
@@ -0,0 +1,45 @@
+let a=10;
+// console.log(a)
+//array variable
+// let arr=[1,2,3,"Dharaneesh",1.5,"19bit074"];
+// for(let i=0;i<arr.length;i++)
+// {
+//   console.log(arr[i])
+// }
+// console.log(arr[2])
+//key and value pair
+//object variable
+let obj={
+  name:"Gopi",
+  phno:"9790642068",
+  email:"dhaarneeshselvaraj@gmail.com",
+  exp:1.5
+}
+// console.log(obj.name)
+//array of objects
+let arrobj=[
+  {
+  name:"Dharaneesh",
+  phno:"9790642068",
+  email:"dhaarneeshselvaraj@gmail.com",
+  exp:1.5
+},
+{
+  name:"Sai",
+  phno:"9790642068",
+  email:"dhaarneeshselvaraj@gmail.com",
+  exp:1.5
+},
+{
+  name:"Gowtham",
+  phno:"9790642068",
+  email:"dhaarneeshselvaraj@gmail.com",
+  exp:1.5
+}
+]
+for(let j=0;j<arrobj.length;j++)
+{
+  console.log(arrobj[j].name)
+  console.log(arrobj[j].phno)
+}
+// console.log(arrobj[2].exp)
