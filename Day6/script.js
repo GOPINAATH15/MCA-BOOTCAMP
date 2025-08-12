ca25a56c710c7ec6f5145081683d1ed43258de6e
GOPINAATH15 <gopinaath515@gmail.com>
2025-08-12 09:03:35 +0530

    Basics of Javascript

---
+++ Day6/script.js
@@ -0,0 +1,87 @@
+// let a=5,b=2;
+// c=a;
+// a=b;
+// b=c;
+// console.log("a is "+a);
+// console.log("b is "+b);
+// console.log("SWAPPED BOTH A and B");
+// console.log("Addition ",a+b);
+// console.log("Subtraction ",a-b);
+// console.log("Division ",a/b);
+// console.log("Multiplication ",a*b);
+// console.log("Modulus ",a%b);
+
+// let a = 123;
+// let c = 123%10;
+// console.log(c);
+// a=a/10;
+// c=a%10;
+// console.log(c);
+// a=a/10;
+// c=a%10;
+// console.log(c);
+// let b=123;
+// console.log(b);
+
+// let a = 123;
+// let sum =0;
+// console.log("---------BEFORE SWAPPING-----------------------")
+// console.log(a)
+// let c= a%10;
+// sum=sum*10+c;//c=3
+// // sum=0*10+3
+// // sum=3
+// a=Math.floor(a/10)//a=12
+// c=a%10;//c=2
+// sum=sum*10+c;
+// //sum=3*10+2
+// //sum=32
+// a=Math.floor(a/10)//a=1
+// c=a%10;
+// sum=sum*10+c;
+// console.log("---------AFTER SWAPPING----------------")
+// console.log(sum)
+
+
+// if(a=-1)
+// {
+//     a=a+1;
+// }
+// if('hello'){
+//     a=a-1;
+// }
+// else{
+//     console.log('Im in home');
+// }
+// if(a)
+// {
+//     console.log("the value is: "+a);
+// }
+
+// let a=10,b=7;
+// let c = a++ + ++a + ++b - b-- + --a + b-- - ++b + a++ + --a - a + b + a + b++ + --b + b;
+// console.log(c);
+
+                                                // to check the given number is odd or even
+// let a=-10;
+
+// if(a/2)
+// {
+//   console.log("The given number is even")
+// }
+// else
+// {
+//   console.log("The given number is odd")
+// }
+                                                // to check the given nnumber is positive or negative
+// if(a>0){
+//   console.log("The given number is Positive")
+// }
+// else if(a<0)
+// {
+//   console.log("The given number is Negative")
+// }
+// else
+// {
+//   cobnsole.log("The given number is Zero")
+// }
