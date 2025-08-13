d0c81425626e54ab175bcb488cfedabf6db44263
GOPINAATH15 <gopinaath515@gmail.com>
2025-08-13 10:40:52 +0530

    methods

---
+++ Day6/mthd.js
@@ -0,0 +1,13 @@
+/*
+There are four types of methods are :
+1. Method without arguments or parameter
+2. Method with arguments or parameter
+3. Arrow Function
+4. CallBack Function
+*/
+function printHello(mytext) {
+    console.log(mytext);
+}
+let text = "hello";
+printHello(text);
+printHello("Welcome");
